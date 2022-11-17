c6b9e1b2ce58ecc49fed6eb9f6bf2694f79b0076
Mafusaillo <danylo.prots.kn.2021@lpnu.ua>
2022-11-17 19:35:03 +0200

    ComplexLab

--- src/main/java/userMenu/MenuCaller.java
+++ src/main/java/userMenu/MenuCaller.java
@@ -1,11 +1,26 @@
 package userMenu;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import static userMenu.Menu.wholeMenu;
 
 public class MenuCaller {
     public static void main(String[] args) throws IOException {
-        wholeMenu();
+        String jdbcURL = "jdbc:postgresql://localhost:5432/TouristCompanyDatabase";
+        String userName = "postgres";
+        String password = "Hajaomija123";
+        try{
+            Connection connection = DriverManager.getConnection(jdbcURL, userName, password);
+            System.out.println("Connected successfully");
+
+            connection.close();
+        } catch (SQLException exception){
+            System.out.println("Connection to database failed, contact help");
+            exception.printStackTrace();
+        }
+//        wholeMenu();
     }
 }
